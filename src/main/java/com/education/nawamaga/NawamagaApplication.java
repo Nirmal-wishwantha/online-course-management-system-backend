e0511348c1bbc476670853a981b0c9e6ab0c9dc6
Nirmal-wishwantha <nirmalwishwantha@gmail.com>
2024-11-25 20:32:27 +0530

    first commit

---
+++ src/main/java/com/education/nawamaga/NawamagaApplication.java
@@ -0,0 +1,13 @@
+package com.education.nawamaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NawamagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NawamagaApplication.class, args);
+	}
+
+}
